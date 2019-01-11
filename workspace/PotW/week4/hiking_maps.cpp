45cce26df3323c62dd84882a404d72f532916722
Danil Ivanov <danilivanov.ivanov@gmail.com>
2019-01-12 00:53:41 +0100

    hiking maps 100%

--- workspace/PotW/week4/hiking_maps.cpp
+++ workspace/PotW/week4/hiking_maps.cpp
@@ -1,107 +1,95 @@
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <iostream>
+#include <stdexcept>
 #include <vector>
 #include <climits>
-#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-typedef K::Triangle_2   Triangle;
-typedef K::Segment_2    Segment;
-typedef K::Point_2      Point;
-typedef K::Line_2       Line;
-typedef std::vector<Point>          pt_vec;
-typedef std::pair<Point, Point>     pt_pair;
-typedef std::vector<pt_pair>        pt_pair_vec;
-typedef std::vector<Triangle>       tri_vec;
+typedef K::Point_2 P;
+typedef K::Segment_2 S;
 
-bool point_in_triangle(const Point &point, const pt_pair_vec triangle) {
-    bool inside = true;
-    for(int i = 0; i < triangle.size(); i++) {
-        inside = inside && !(CGAL::right_turn(triangle[i].first, triangle[i].second, point));
-    }
-    return inside;
-}
-
-void testcase(){
+void testcase() {
     int m, n; std::cin >> m >> n;
 
-    pt_pair_vec paths(m-1);
-    int p_x, p_y; std::cin >> p_x >> p_y;
-    Point p_start(p_x, p_y);
-    for(int i = 0; i < m-1; i++) {
-        std::cin >> p_x >> p_y;
-        Point p_end(p_x, p_y);
-        paths[i] = std::make_pair(p_start, p_end);
-        p_start = p_end;
+    // Read in points
+    std::vector<P> path_points(m);
+    for(int i = 0; i < m; i++) {
+        int x, y; std::cin >> x >> y;
+        path_points[i] = P(x, y);
     }
 
-    // If for a pair of points, both lie in the triangle, save i
-    std::vector<std::vector<int> > path_to_tri(m-1, std::vector<int>(0));
-    for(int triangle_idx = 0; triangle_idx < n; ++triangle_idx){
-        // Read the 6 points that describe the lines that form a triangle
-        pt_pair_vec lines(3);
-        for (int j = 0; j < 3; ++j) {
+    // Construct hiking path
+    std::vector<S> path(m);
+    for (int i = 0; i < m - 1; i++) {
+        path[i] = S(path_points[i], path_points[i + 1]);
+    }
+
+    // Read in map parts - for each find all the path legs it covers
+    std::map<int, std::vector<int>> map_coverage;
+    for (int i = 0; i < n; i++) {
+        // Read in the map delimiting lines
+        std::vector<std::pair<P, P>> points(3);
+        for(int tri_pt = 0; tri_pt < 3; tri_pt++) {
             int x1, y1, x2, y2; std::cin >> x1 >> y1 >> x2 >> y2;
-            lines[j] = std::make_pair(Point(x1, y1), Point(x2, y2));
+            points[tri_pt] = std::make_pair(P(x1, y1), P(x2, y2));
         }
 
-        // Order the points in pairs s.t. (p, p'), (q, q'), (r, r') performs a counter-clockwise
-        // walk around the triangle formed by the lines
-        // So if p, p', q form a right turn, swap p and p'
-        // If q, q', r form a right turn, swap q and q'
-        // If r, r', p form a right turn, swap r and r'
-        for(int j = 0; j < 3; ++j) {
-            pt_pair l1 = lines[j];
-            pt_pair l2 = lines[(j + 1) % 3];
-            if (CGAL::right_turn(l1.first, l1.second, l2.first)) {
-                lines[j] = std::make_pair(l1.second, l1.first);
-            } else if (CGAL::right_turn(l1.first, l1.second, l2.second)) {
-                lines[j] = std::make_pair(l1.second, l1.first);
+        // Make sure the order of the points is okay, correct if needed.
+        for(int tri_del = 0; tri_del < 3; tri_del++) {
+            P p1, p2, p3; 
+            p1 = points[tri_del].first;
+            p2 = points[tri_del].second;
+            p3 = points[(tri_del + 1) % 3].first;
+            // Swap p1 and p2 if p1 p2 p3 don't form a left turn
+            if (CGAL::right_turn(p1, p2, p3)) {
+                points[tri_del].first = p2; 
+                points[tri_del].second = p1; 
             }
         }
 
-        // Compute which paths are fully covered by the triangle
-        for(int path_idx = 0; path_idx < m-1; ++path_idx) {
-            bool source_in = point_in_triangle(paths[path_idx].first, lines);
-            bool target_in = point_in_triangle(paths[path_idx].second, lines);
-            if(source_in && target_in) {
-                path_to_tri[path_idx].push_back(triangle_idx);
+        // Compute which legs are contained by this map part
+        for(int p = 0; p < m - 1; p++) {
+            bool covered = true;
+            // Check if path p is covered by map part i
+            for(int tri_del = 0; tri_del < 3; tri_del++) {
+                P p1, p2; 
+                p1 = points[tri_del].first;
+                p2 = points[tri_del].second;
+                covered &= !(CGAL::right_turn(p1, p2, path[p].source()) ||
+                      CGAL::right_turn(p1, p2, path[p].target()));
+            }
+            if (covered) {
+               map_coverage[i].push_back(p);
             }
         }
     }
 
-    // Sliding window
-    std::vector<int> index(m-1, 0);
-    bool end_not_reached = true;
-    int min_tri_cost = INT_MAX;
-    while(end_not_reached) {
-        int min_tri = INT_MAX, max_tri = INT_MIN;
-        int min_path_idx = INT_MAX;
-        for (int i = 0; i < m - 1; i++) {
-            int i_path_tri = path_to_tri[i][index[i]];
-            if (i_path_tri < min_tri) {
-                min_tri = i_path_tri;
-                min_path_idx = i; 
-            }
-            if (max_tri < i_path_tri) {
-                max_tri = i_path_tri;
-            }
-        }
+    // Taken and adapted from master solution
+    std::vector<int> covered(m - 1, 0);
+    std::size_t uncovered = m - 1;
+    std::size_t best = n;
+    for (std::size_t tb = 0, te = 0; tb != map_coverage.size();) {
+        // Get coverage by incrementing te
+        for (; uncovered > 0 && te != map_coverage.size(); te++) {
+            for (int j = 0; j < map_coverage[te].size(); j++) {
+                if (++covered[map_coverage[te][j]] == 1) --uncovered;
+            } 
+        } 
+        if (uncovered != 0) break;
 
-        int curr_cost = max_tri - min_tri + 1;
-        if (curr_cost < min_tri_cost) {
-            min_tri_cost = curr_cost;
-        }
-
-        if (++index[min_path_idx] == path_to_tri[min_path_idx].size()) {
-            end_not_reached = false;
-        }
+        do {
+            for (int j = 0; j < map_coverage[tb].size(); j++) {
+                if (--covered[map_coverage[tb][j]] == 0) ++uncovered;
+            }
+        } while (++tb != te && uncovered == 0);
+        best = std::min(best, te-tb+1);
     }
-
-    std::cout << min_tri_cost << std::endl;
-
+    
+    std::cout << best << std::endl;
 }
 
-int main(){
+int main() {
+    std::ios_base::sync_with_stdio(false);
     int t; std::cin >> t;
     while(t--) testcase();
     return 0;
