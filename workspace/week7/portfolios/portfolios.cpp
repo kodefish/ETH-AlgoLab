78511970c2809bb362621ee3e136f3d4fc5c6217
Danil Ivanov <danilivanov.ivanov@gmail.com>
2019-01-14 23:23:20 +0100

    rewrite of portfolios

--- workspace/week7/portfolios/portfolios.cpp
+++ workspace/week7/portfolios/portfolios.cpp
@@ -1,72 +1,55 @@
 #include <iostream>
-
+#include <vector>
+#include <cassert>
 #include <CGAL/basic.h>
 #include <CGAL/QP_models.h>
 #include <CGAL/QP_functions.h>
 #include <CGAL/Gmpq.h>
 
-// Exact rational type
 typedef CGAL::Gmpq ET;
 
-// Program and solution types
-typedef CGAL::Quadratic_program<ET> Program;
+typedef CGAL::Quadratic_program<int> Program;
 typedef CGAL::Quadratic_program_solution<ET> Solution;
 
-void testcase(int num_assets, int num_investors) {
-
-  // Non-negative QP with Ax >= p
-  Program qp(CGAL::LARGER, true, 0, false, 0);
-  const int COST = 0;
-  const int RETURN = 1;
-
-  // Override defaults
-  qp.set_r(COST, CGAL::SMALLER); // cost of portfolio <= capital of investor
-  qp.set_r(RETURN, CGAL::LARGER); // return of portfolio >= min return for investor
-
-  // Read in cost c_i and expected return r_i of each asset 
-  for (int i = 0; i < num_assets; i++) {
-    int c_i, r_i; std::cin >> c_i >> r_i;
-    qp.set_a(i, COST, c_i);
-    qp.set_a(i, RETURN, r_i);
-  }
-
-  // Read in covariance matrix D - works, because D is positive semi definite
-  for (int i = 0; i < num_assets; i++) {
-    for (int j = 0; j < num_assets; j++) {
-      int v_i_j; std::cin >> v_i_j;
-      qp.set_d(i, j, 2 * v_i_j);
-    }
-  }
-
-  // Find portfolio for each investor
-  for (int i = 0; i < num_investors; i++) {
-    int C, R, V; std::cin >> C >> R >> V;
-
-    // Set rhs of constraint equations
-    qp.set_b(COST, C);
-    qp.set_b(RETURN, R);
-
-    // Solve
-    Solution s = CGAL::solve_nonnegative_quadratic_program(qp, ET());
-    if (s.is_optimal()) {
-      ET variance = s.objective_value().numerator() / s.objective_value().denominator();
-      if (variance <= V) {
-	std::cout << "Yes." << std::endl;
-      } else {
-	std::cout << "No." << std::endl;
-      }
-    } else {
-      std::cout << "No." << std::endl;
-    }
-  }
-}
-
 int main() {
-  std::ios_base::sync_with_stdio(false);
-  int n, m; std::cin >> n >> m;
-  while (n != 0 && m != 0) {
-    testcase(n, m);
-    std::cin >> n >> m;
-  }
-  return 0;
+    while (true) {
+        int n, m; std::cin >> n >> m;
+        if (n == 0 && m == 0) break;
+
+        Program lp(CGAL::SMALLER, true, 0, false, 0);
+
+        const int COST = 0;
+        const int RETURN = 1;
+        for (int i = 0; i < n; i++) {
+            int c, r; std::cin >> c >> r; 
+            lp.set_a(i, COST, c);
+            lp.set_a(i, RETURN, -r);
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int c_ij; std::cin >> c_ij; 
+                if (j <= i)
+                    lp.set_d(i, j, 2*c_ij);
+            }
+        }
+
+        for(int i = 0; i < m; i++) {
+            int C, R, V; std::cin >> C >> R >> V;
+            
+            lp.set_b(COST, C);
+            lp.set_b(RETURN, -R);
+
+            Solution s = CGAL::solve_quadratic_program(lp, ET());
+            assert(s.solves_quadratic_program);
+
+            if (s.is_optimal()) {
+                std::cout << 
+                        ((s.objective_value().numerator() /  s.objective_value().denominator()) <= V ? "Yes." : "No.")  << std::endl;
+            } else {
+                std::cout << "No." << std::endl; 
+            }
+
+        }
+    }
 }
