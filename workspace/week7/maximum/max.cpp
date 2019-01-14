a31b1ca5e0aa32c42c0df8f407413845c55f4c35
Danil Ivanov <danilivanov.ivanov@gmail.com>
2019-01-14 16:39:46 +0100

    redid maximize it

--- workspace/week7/maximum/max.cpp
+++ workspace/week7/maximum/max.cpp
@@ -5,16 +5,12 @@
 #include <CGAL/QP_functions.h>
 #include <CGAL/Gmpq.h>
 
-// Choose exact integral type
 typedef CGAL::Gmpq ET;
 
-// Program and solution types
-typedef CGAL::Quadratic_program<ET> Program;
+typedef CGAL::Quadratic_program<int> Program;
 typedef CGAL::Quadratic_program_solution<ET> Solution;
 
-typedef CGAL::Quotient<ET> SolT;
-
-double ceil_to_double (const SolT &x) {
+double ceil_to_double (const CGAL::Quotient<ET> &x) {
     double a = std::ceil(CGAL::to_double(x));
     while (a < x) a++;
     while (a - 1 >= x) a--;
@@ -22,49 +18,66 @@ double ceil_to_double (const SolT &x) {
 }
 
 int main() {
-  int p; std::cin >> p;
-  while(p != 0) {
+    while (true) {
+        int p, a, b; std::cin >> p >> a >> b;
+        if (p == 0) return 0;
 
-    int a, b; std::cin >> a >> b;
-    const int X = 0;
-    const int Y = 1;
-    const int Z = 2; // Rememeber to take sqrt of Z!!!
-    Program qp;
-    int mult = 1;
+        // Construct the problem, depending on p
+        Program lp;
+        int neg = 1;
+        if (p == 1) {
+            lp = Program(CGAL::SMALLER, true, 0, false, 0);
 
-    if (p == 1) {
-      // First case
-      qp = Program(CGAL::SMALLER, true, 0, false, 0); // x, y >= 0
-      qp.set_a(X, 0, 1); qp.set_a(Y, 0, 1); qp.set_b(0, 4); // x + y <= 4
-      qp.set_a(X, 1, 4); qp.set_a(Y, 1, 2); qp.set_b(1, a * b); // 4x + 2y <= ab
-      qp.set_a(X, 2, -1); qp.set_a(Y, 2, 1); qp.set_b(2, 1); // -x + y <= 1
-      qp.set_c(Y, -b); qp.set_d(X, X, 2 * a); // min(-by + ax^2) - DON'T FORGET TO MULTIPLY BY -1 AFTER!
+            // Variables x and y
+            const int X = 0; 
+            const int Y = 1; 
 
-      mult = -1;
-    } else {
-      // Second case
-      qp = Program(CGAL::LARGER, false, 0, false, 0); // x, y >= 0
-      qp.set_a(X, 0, 1); qp.set_a(Y, 0, 1); qp.set_b(0, -4); // x + y >= -4
-      qp.set_a(X, 1, 4); qp.set_a(Y, 1, 2); qp.set_a(Z, 1, 1); qp.set_b(1, - (a * b)); // 4x + 2y + z^2 >= -ab
-      qp.set_a(X, 2, -1); qp.set_a(Y, 2, 1); qp.set_b(2, -1); // -x + y >= -1
-      qp.set_d(X, X, 2 * a); qp.set_c(Y, b); qp.set_d(Z, Z, 2); // min(ax^2 + by + z^4)
+            // Setup constraints
+            // -x - y <= 0
+            lp.set_a(X, 0, 1); lp.set_a(Y, 0, 1); lp.set_b(0, 4); 
+            // 4x + 2y <= ab
+            lp.set_a(X, 1, 4); lp.set_a(Y, 1, 2); lp.set_b(1, a*b); 
+            // -x + y <= 1
+            lp.set_a(X, 2, -1); lp.set_a(Y, 2, 1); lp.set_b(2, 1); 
+            
+            // Setup objective function
+            lp.set_d(X, X, 2*a); 
+            lp.set_c(Y, -b);
+            neg = -1; // used later because maximization
+        } else if (p == 2) {
+            lp = Program(CGAL::LARGER, false, 0, false, 0);
 
-      qp.set_u(X, true, 0); // x <= 0
-      qp.set_u(Y, true, 0); // y <= 0
+            // Variables
+            const int X = 0; 
+            const int Y = 1; 
+            const int Z = 2; 
+
+            // Constraints
+            // x + y >= -4
+            lp.set_a(X, 0, 1); lp.set_a(Y, 0, 1); lp.set_b(0, -4);
+            // 4x + 2y + z^2 >= -ab
+            lp.set_a(X, 1, 4); lp.set_a(Y, 1, 2); lp.set_a(Z, 1, 1); lp.set_b(1, -a*b);
+            // -x + y >= -1
+            lp.set_a(X, 2, -1); lp.set_a(Y, 2, 1); lp.set_b(2, -1);
+            // x, y <= 0
+            lp.set_u(X, true, 0); lp.set_u(Y, true, 0);
+
+            // Objective function
+            lp.set_d(X, X, 2 * a);
+            lp.set_d(Z, Z, 2);
+            lp.set_c(Y, b);
+        }
+
+        // Print solution
+        Solution s = CGAL::solve_quadratic_program(lp, ET());
+        assert(s.solves_quadratic_program());
+        if (!s.is_optimal()) {
+            std::cout << "no" << std::endl; 
+        } else if (s.is_unbounded()) {
+            std::cout << "unbounded" << std::endl; 
+        } else {
+            std::cout << std::setprecision(12) << neg * ceil_to_double(s.objective_value()) << std::endl; 
+        }
+
     }
-
-    Solution s = CGAL::solve_quadratic_program(qp, ET());
-    assert(s.solves_quadratic_program(qp));
-    if (!s.is_optimal()) {
-	std::cout << "no" << std::endl;
-    } else if (s.is_unbounded()) {
-	std::cout << "unbounded" << std::endl;
-    } else {
-      std::cout << std::setprecision(12) << mult * ceil_to_double(s.objective_value()) << std::endl;
-    }
-
-    std::cin >> p;
-
-  }
-  return 0;
 }
