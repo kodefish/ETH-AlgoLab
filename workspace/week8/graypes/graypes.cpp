10a4ec09cd47d294594752d3a369cf07a8907989
Danil Ivanov <danilivanov.ivanov@gmail.com>
2019-01-16 15:08:44 +0100

    rewrote graypes

--- workspace/week8/graypes/graypes.cpp
+++ workspace/week8/graypes/graypes.cpp
@@ -1,49 +1,34 @@
-#include <iostream>
-#include <vector>
-#include <climits>
-#include <cmath>
-
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Delaunay_triangulation_2.h>
-#include <CGAL/squared_distance_2.h>
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-typedef CGAL::Delaunay_triangulation_2<K> Triangulation;
+typedef CGAL::Delaunay_triangulation_2<K>  Triangulation;
+typedef Triangulation::Edge_iterator  Edge_iterator;
 
-void testcase(int num_apes) {
-  // Read in points
-  std::vector<K::Point_2> pts;
-  for(int i = 0; i < num_apes; i++) {
-    int x, y; std::cin >> x >> y;
-    pts.push_back(K::Point_2(x, y));
+int main()
+{
+    while(true) {
+        // read number of points
+        std::size_t n;
+        std::cin >> n;
+        if (n == 0) break;
+
+        // read points
+        std::vector<K::Point_2> pts;
+        pts.reserve(n);
+        for (std::size_t i = 0; i < n; ++i) {
+            K::Point_2 p;
+            std::cin >> p;
+            pts.push_back(p);
+        }
+        // construct triangulation
+        Triangulation t;
+        t.insert(pts.begin(), pts.end());
+        // output all edges
+        K::FT min_length = INT_MAX;
+        for (Edge_iterator e = t.finite_edges_begin(); e != t.finite_edges_end(); ++e)
+            min_length = std::min(min_length, t.segment(e).squared_length());
+        
+        std::cout << std::ceil(std::sqrt(min_length) * 50) << std::endl;
+        }
   }
-
-  
-
-  // Construct triangulation
-  Triangulation t;
-  t.insert(pts.begin(), pts.end());
-
-  double min_time = INT_MAX;
-  for (Triangulation::Edge_iterator e_itr = t.finite_edges_begin(); e_itr != t.finite_edges_end(); e_itr++){
-    Triangulation::Vertex_handle v1 = e_itr->first->vertex((e_itr->second + 1) % 3);
-    Triangulation::Vertex_handle v2 = e_itr->first->vertex((e_itr->second + 2) % 3);
-
-    double time = CGAL::squared_distance(v1->point(), v2->point());
-    if (time < min_time) {
-      min_time = time;
-    }
-  }
-
-  // Divide time by 2 and convert to hundreth of a second -> time/(2 * 0.01) = time * 50
-  std::cout << std::ceil(std::sqrt(min_time) * 50) << std::endl;
-}
-
-int main() {
-  int n; std::cin >> n;
-  while (n != 0) {
-    testcase(n);
-    std::cin >> n;
-  }
-  return 0;
-}
