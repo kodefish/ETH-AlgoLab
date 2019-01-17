bb019d4ca5bb72b0c7fa7e6e2bfabf0ca2db53d4
Danil Ivanov <danilivanov.ivanov@gmail.com>
2019-01-17 23:54:33 +0100

    h1n1 80 and germs 100

--- workspace/week8/bistro/bistro.cpp
+++ workspace/week8/bistro/bistro.cpp
@@ -1,37 +1,38 @@
-#include <iostream>
-#include <vector>
-
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Delaunay_triangulation_2.h>
-#include <CGAL/squared_distance_2.h>
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-typedef CGAL::Delaunay_triangulation_2<K> Triangulation;
+typedef CGAL::Delaunay_triangulation_2<K>  Triangulation;
 
-void testcase(int num_existing) {
-  std::vector<K::Point_2> pts;
-  pts.reserve(num_existing);
-  for(int i = 0; i < num_existing; i++) {
-    int x, y; std::cin >> x >> y;
-    pts.push_back(K::Point_2(x, y));
-  }
-
-  Triangulation t;
-  t.insert(pts.begin(), pts.end());
-
-  int num_locations; std::cin >> num_locations;
-  for(int i = 0; i < num_locations; i++) {
-    K::Point_2 pt, nearest; std::cin >> pt;
-    nearest = t.nearest_vertex(pt)->point();
-    std::cout << std::setprecision(24) << CGAL::squared_distance(nearest, pt) << std::endl;
-  }
+int main()
+{
+    while (true) {
+        std::ios_base::sync_with_stdio(false);
+        // read number of points
+        std::size_t n;
+        std::cin >> n;
+        if (n == 0) break;
+    
+        // read points
+        std::vector<K::Point_2> pts;
+        pts.reserve(n);
+        for (std::size_t i = 0; i < n; ++i) {
+            K::Point_2 p;
+            std::cin >> p;
+            pts.push_back(p);
+        }
+    
+        // construct triangulation
+        Triangulation t;
+        t.insert(pts.begin(), pts.end());
+    
+        // read in potential locations
+        int m; std::cin >> m;
+        for (int i = 0; i < m; i++) {
+            K::Point_2 p; 
+            std::cin >> p; 
+            std::cout << std::setprecision(24) << CGAL::squared_distance(t.nearest_vertex(p)->point(), p) << std::endl;
+        }
+    }
 }
 
-int main() {
-  std::ios_base::sync_with_stdio(false);
-  int num_existing; std::cin >> num_existing;
-  while(num_existing != 0) {
-    testcase(num_existing);
-    std::cin >> num_existing;
-  }
-}
