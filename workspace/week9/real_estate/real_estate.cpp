878fa9f9447121ad90e97ad7754f4e158f8e4945
Danil Ivanov <danilivanov.ivanov@gmail.com>
2019-01-18 18:56:46 +0100

    real estate

---
+++ workspace/week9/real_estate/real_estate.cpp
@@ -0,0 +1,117 @@
+// ALGOLAB BGL Tutorial 3
+// Code snippets demonstrating 
+// - MinCostMaxFlow with arbitrary edge costs using cycle_canceling
+// - MinCostMaxFlow with non-negative edge costs using successive_shortest_path_nonnegative_weights
+
+// Compile and run with one of the following:
+// g++ -std=c++11 -O2 bgl_mincostmaxflow.cpp -o bgl_mincostmaxflow; ./bgl_mincostmaxflow
+// g++ -std=c++11 -O2 -I path/to/boost_1_58_0 bgl_mincostmaxflow.cpp -o bgl_mincostmaxflow; ./bgl_mincostmaxflow
+
+// Includes
+// ========
+// STL includes
+#include <iostream>
+#include <cstdlib>
+// BGL includes
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/cycle_canceling.hpp>
+#include <boost/graph/push_relabel_max_flow.hpp>
+#include <boost/graph/successive_shortest_path_nonnegative_weights.hpp>
+#include <boost/graph/find_flow_cost.hpp>
+
+// BGL Graph definitions
+// ===================== 
+// Graph Type with nested interior edge properties for Cost Flow Algorithms
+typedef boost::adjacency_list_traits<boost::vecS, boost::vecS, boost::directedS> Traits;
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, boost::no_property,
+    boost::property<boost::edge_capacity_t, long,
+        boost::property<boost::edge_residual_capacity_t, long,
+            boost::property<boost::edge_reverse_t, Traits::edge_descriptor,
+                boost::property <boost::edge_weight_t, long> > > > > Graph; // new!
+// Interior Property Maps
+typedef boost::property_map<Graph, boost::edge_capacity_t>::type      EdgeCapacityMap;
+typedef boost::property_map<Graph, boost::edge_weight_t >::type       EdgeWeightMap; // new!
+typedef boost::property_map<Graph, boost::edge_residual_capacity_t>::type ResidualCapacityMap;
+typedef boost::property_map<Graph, boost::edge_reverse_t>::type       ReverseEdgeMap;
+typedef boost::graph_traits<Graph>::vertex_descriptor          Vertex;
+typedef boost::graph_traits<Graph>::edge_descriptor            Edge;
+typedef boost::graph_traits<Graph>::out_edge_iterator  OutEdgeIt; // Iterator
+
+// Custom Edge Adder Class, that holds the references
+// to the graph, capacity map, weight map and reverse edge map
+// ===============================================================
+class EdgeAdder {
+    Graph &G;
+    EdgeCapacityMap &capacitymap;
+    EdgeWeightMap &weightmap;
+    ReverseEdgeMap  &revedgemap;
+
+public:
+    EdgeAdder(Graph &G, EdgeCapacityMap &capacitymap, EdgeWeightMap &weightmap, ReverseEdgeMap &revedgemap) 
+        : G(G), capacitymap(capacitymap), weightmap(weightmap), revedgemap(revedgemap) {}
+
+    void addEdge(int u, int v, long c, long w) {
+        Edge e, rev_e;
+        boost::tie(e, boost::tuples::ignore) = boost::add_edge(u, v, G);
+        boost::tie(rev_e, boost::tuples::ignore) = boost::add_edge(v, u, G);
+        capacitymap[e] = c;
+        weightmap[e] = w; // new!
+        capacitymap[rev_e] = 0;
+        weightmap[rev_e] = -w; // new
+        revedgemap[e] = rev_e; 
+        revedgemap[rev_e] = e; 
+    }
+};
+
+void testcase() {
+    int N, M, S;
+    std::cin >> N >> M >> S;
+
+    // Create Graph and Maps
+    Graph G(N + M + S + 2);
+    const int v_source = N + M + S;
+    const int v_target = v_source + 1;
+    const int sites = N, states = N + M;
+    EdgeCapacityMap capacitymap = boost::get(boost::edge_capacity, G);
+    EdgeWeightMap weightmap = boost::get(boost::edge_weight, G);
+    ReverseEdgeMap revedgemap = boost::get(boost::edge_reverse, G);
+    ResidualCapacityMap rescapacitymap = boost::get(boost::edge_residual_capacity, G);
+    EdgeAdder eaG(G, capacitymap, weightmap, revedgemap);
+    
+    // Read in state limits
+    for (int i = 0; i < S; i++) {
+        int state_limit; std::cin >> state_limit; 
+        eaG.addEdge(states + i, v_target, state_limit, 0);
+    }
+
+    // Read in all the sites and link them to their state
+    for (int i = 0; i < M; i++) {
+        int state; std::cin >> state;
+        eaG.addEdge(sites + i, states + state - 1, 1, 0);
+    }
+
+    // Read in buyer bids
+    for (int i = 0; i < N; i++) {
+        eaG.addEdge(v_source, i, 1, 0);
+        for (int j = 0; j < M; j++) {
+            int bid; std::cin >> bid;
+            eaG.addEdge(i, sites + j, 1, -bid);
+        }
+    }
+
+    // Run the algorithm
+
+    // Option 1: Min Cost Max Flow with cycle_canceling
+    int flow = boost::push_relabel_max_flow(G, v_source, v_target);
+    boost::cycle_canceling(G);
+    int cost = boost::find_flow_cost(G);
+    std::cout << flow << " " << -cost << std::endl;
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    int t; std::cin >> t;
+    while (t--) testcase();
+    return 0;
+}
+
