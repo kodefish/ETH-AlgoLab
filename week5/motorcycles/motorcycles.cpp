5ab9799a81bcb2b99c4a2a6e878e62205e6c64c5
Danil Ivanov <danilivanov.ivanov@gmail.com>
2019-10-19 18:09:01 +0200

    motorcycles full points

--- week5/motorcycles/motorcycles.cpp
+++ week5/motorcycles/motorcycles.cpp
@@ -6,15 +6,19 @@
 typedef CGAL::Exact_predicates_exact_constructions_kernel K;
 typedef K::Line_2 Line;
 typedef K::Point_2 Point;
+typedef K::Ray_2 Ray;
 
 struct BikerTrajectory {
     long source;
     K::FT slope;
     int index;
-    
+    Ray trajectory;
+
     //    Order in descending order of y0 (highest point first)
     bool operator<(const BikerTrajectory& t) const { return this->source < t.source;  }
 };
+
+bool sort_by_index(const BikerTrajectory& t1, const BikerTrajectory& t2) { return t1.index < t2.index;  }
 
 void testcase() {
     // Read input
@@ -27,29 +31,47 @@ void testcase() {
         Line l(Point(0, y0), Point(x1, y1));
 
         // Create ray
-        biker_trajectories.push_back(BikerTrajectory{y0, -l.a()/l.b(), i});
+        biker_trajectories.push_back(BikerTrajectory{y0, -l.a()/l.b(), i, Ray(Point(0, y0), Point(x1, y1))});
     }
 
-    // Sort bikers in descending order of y0
+    // Sort bikers in ascending order of y0
     std::sort(biker_trajectories.begin(), biker_trajectories.end());
 
-    // Choose a random biker and follow him until we hit an intersection
-    // Stay on the biker that got to the intersection first, delete the one that got there second
-    // Keep track of who rides off, they all start with hearts full of hope
-    std::vector<bool> biker_rides_off(n, true);
-    int last_collision = 0;
-    for (int i = 0; i < n; i++) {
-        K::FT si = biker_trajectories[i].slope;
-        for (int j = 0; j < i; j++) {
-            if (biker_rides_off[biker_trajectories[j].index]) {
-                K::FT sj = biker_trajectories[j].slope;
-                biker_rides_off[biker_trajectories[j].index] = biker_rides_off[biker_trajectories[j].index] && si >= sj;
+    // Start at bottom, and work our way up by checking for intersections with bikers beneath us
+    // Keep track of bikers that are riding in a stack (the higher the source, the further up in the stack)
+    std::vector<BikerTrajectory> biker_rides_off;
+    biker_rides_off.push_back(biker_trajectories[0]);
+    int i = 1;
+    while (i < n) {
+        BikerTrajectory bi = biker_trajectories[i];
+        bool keeps_riding = true;
+        while(!biker_rides_off.empty() && keeps_riding) {
+            BikerTrajectory bj = biker_rides_off.back();
+            if (CGAL::do_intersect(bi.trajectory, bj.trajectory)) {
+                if (bi.slope + bj.slope > 0) {
+                    // Biker i has smaller slope than biker j -> biker i is closer to straight line and therefore was
+                    // at the intersection first
+                    biker_rides_off.pop_back();
+                } else {
+                    // If slopes are equal -> biker i is coming from the top therefore he yields
+                    // If slope of biker i is larger, then he got to the intersectoin second and stops
+                    keeps_riding = false;
+                }
+            } else {
+                break;
             }
         }
+        if (keeps_riding) {
+            biker_rides_off.push_back(bi);
+        }
+        i++;
     }
 
-    for (int i = 0; i < n; i++) {
-        if (biker_rides_off[i]) std::cout << i << " ";
+    // Sort riding bikers by index
+    std::sort(biker_rides_off.begin(), biker_rides_off.end(), sort_by_index);
+
+    for (auto biker = biker_rides_off.begin(); biker != biker_rides_off.end(); biker++) {
+        std::cout << biker->index << " ";
     }
     std::cout << std::endl;
 }
