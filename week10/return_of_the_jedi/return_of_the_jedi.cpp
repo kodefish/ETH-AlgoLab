2a266a2fd09811b215765fe4f3a96add2f977237
Danil Ivanov <danilivanov.ivanov@gmail.com>
2020-01-13 12:41:05 +0100

    return of the jedi 100

--- week10/return_of_the_jedi/return_of_the_jedi.cpp
+++ week10/return_of_the_jedi/return_of_the_jedi.cpp
@@ -1,84 +1,114 @@
-// STL includes
+// Full credit goes to YaweiYe29
+//  STL includes
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <climits>
+#include <stack>
 
 // BGL includes
 #include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/kruskal_min_spanning_tree.hpp>
+#include <boost/graph/prim_minimum_spanning_tree.hpp>
 
 typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS,
-  boost::no_property, boost::property<boost::edge_weight_t, int> >      weighted_graph;
-typedef boost::property_map<weighted_graph, boost::edge_weight_t>::type weight_map;
-typedef boost::graph_traits<weighted_graph>::edge_descriptor            edge_desc;
-typedef boost::graph_traits<weighted_graph>::vertex_descriptor          vertex_desc;
+                              boost::no_property,
+                              boost::property<boost::edge_weight_t, int>> Graph;
+typedef boost::graph_traits<Graph>::edge_descriptor Edge;
+typedef boost::graph_traits<Graph>::vertex_descriptor Vertex;
+typedef boost::graph_traits<Graph>::edge_iterator EdgeIt;
+typedef boost::graph_traits<Graph>::out_edge_iterator OutEdgeIt;
+typedef boost::property_map<Graph, boost::edge_weight_t>::type WeightMap;
+
+void preprocess(int from, Graph &G, int n, std::vector<std::vector<int>> &weights, std::vector<std::vector<int>> &preprocessed) {
+    std::vector<bool> visited(n, false);
+    std::stack<int> dfs;
+    dfs.push(from);
+    visited[from] = true;
+    preprocessed[from][from] = 0;
+
+    // dfs
+    while(!dfs.empty()) {
+        int curr = dfs.top();
+        dfs.pop();
+        OutEdgeIt oebeg, oeend;
+        for (boost::tie(oebeg, oeend) = boost::out_edges(curr, G); oebeg != oeend; oebeg++) {
+            Vertex u = boost::source(*oebeg, G);
+            Vertex v = boost::target(*oebeg, G);
+            if (visited[v]) continue;
+            int w = weights[u][v];
+            preprocessed[from][v] = std::max(w, preprocessed[from][curr]);
+            visited[v] = true;
+            dfs.push(v);
+        }
+    }
+}
 
 void testcase() {
-    // Read in info
-    int n, i;
-    std::cin >> n >> i;
+    int n, root; std::cin >> n >> root;
+    Graph G(n);
+    WeightMap weightmap = boost::get(boost::edge_weight, G);
+    std::vector<std::vector<int>> weights(n, std::vector<int>(n, -1));
 
-    // compute original graph
-    weighted_graph G(n);
-    weight_map weights = boost::get(boost::edge_weight, G);
-
-    std::vector<int> costs;
-
-    for (int j = 1; j < n; j++) {
-        for (int k = 1; k < n - j + 1; k++) {
+    // Create graph from input
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
             int c; std::cin >> c;
-            costs.push_back(c);
-
-            // Add edge to graph
-            edge_desc e;
-            e = boost::add_edge(j-1, j+k-1, G).first; weights[e]=c;
+            Edge e; bool b;
+            boost::tie(e, b) = boost::add_edge(i, j, G);
+            weightmap[e] = c;
+            weights[i][j] = weights[j][i] = c;
         }
     }
 
-    // compute MST leia style
-    std::vector<edge_desc> mst;    // vector to store MST edges (not a property map!)
-    boost::kruskal_minimum_spanning_tree(G, std::back_inserter(mst));
+    // Prim MST from root
+    std::vector<Vertex> prim_pred_map(n);
+    boost::prim_minimum_spanning_tree(G,
+                                      boost::make_iterator_property_map(prim_pred_map.begin(), boost::get(boost::vertex_index, G)),
+                                      boost::root_vertex(root-1));
 
-    //std::cerr << "Leia MST" << std::endl;
-    for (std::vector<edge_desc>::iterator it = mst.begin(); it != mst.end(); ++it) {
-        //std::cerr << "\t" << boost::source(*it, G) << " " << boost::target(*it, G) << std::endl;
-    }
-    
-    int min_mst_cost = INT_MAX;
+    Graph G2(n);
+    std::vector<std::vector<bool>> added = std::vector<std::vector<bool>>(n, std::vector<bool>(n, false));
+    std::vector<std::pair<int, int>> non_mst;
 
-    std::set<int> mst_costs;
-
-    // Remove one edge from MST and try computing new one, keep best version
-    for (std::vector<edge_desc>::iterator it = mst.begin(); it != mst.end(); ++it) {
-        //std::cerr << "removing " << boost::source(*it, G) << " " << boost::target(*it, G) << "... " << std::endl;
-        int source = boost::source(*it, G), target = boost::target(*it, G);
-        boost::remove_edge(source, target, G);
-        std::vector<edge_desc> new_mst;    // vector to store MST edges (not a property map!)
-        boost::kruskal_minimum_spanning_tree(G, std::back_inserter(new_mst));
-
-        int mst_cost = 0;
-        //std::cerr << "Modified MST" << std::endl;
-        for (std::vector<edge_desc>::iterator it = new_mst.begin(); it != new_mst.end(); ++it) {
-            //std::cerr << "\t" << boost::source(*it, G) << " " << boost::target(*it, G) << std::endl;
-            mst_cost += weights[*it];
+    int weight = 0;
+    for (int i = 0; i < n; i++) {
+        Vertex u = i;
+        OutEdgeIt oebeg, oeend;
+        for (boost::tie(oebeg, oeend) = boost::out_edges(u, G); oebeg != oeend; oebeg++) {
+            Vertex v = boost::target(*oebeg, G);
+            if (prim_pred_map[u] == v) {
+                weight += weights[u][v];
+                Edge e; bool s;
+                boost::tie(e, s) = boost::add_edge(u, v, G2);
+            } else if (prim_pred_map[v] != u) {
+                if (!added[u][v] && !added[v][u]) {
+                    added[u][v] = true;
+                    non_mst.push_back(std::make_pair(u, v));
+                }
+            }
         }
-
-        mst_costs.insert(mst_cost);
-
-        //std::cerr << "\tcost: " << mst_cost << std::endl;
-
-        if (mst_cost < min_mst_cost) min_mst_cost = mst_cost;
-        boost::add_edge(source, target, G);
     }
 
-    for (auto it = mst_costs.begin(); it != mst_costs.end(); it++) std::cerr << *it << " ";
-    std::cerr << std::endl;
+    // Find replacement of min cost
+    std::vector<std::vector<int>> preprocessed(n, std::vector<int>(n, -1));
 
-    std::cout << min_mst_cost << std::endl;
+    int min = INT32_MAX;
+    for (auto it = non_mst.begin(); it != non_mst.end(); it++) {
+        int w = weights[it->first][it->second];
+        if (preprocessed[it->first][0] == -1) {
+            preprocess(it->first, G2, n, weights, preprocessed);
+        }
+        int w2 = w - preprocessed[it->first][it->second];
+        if (w2 < min) min = w2;
+    }
+
+    std::cout << weight + min << std::endl;
+
 }
 
 int main() {
     std::ios_base::sync_with_stdio(false);
     int t; std::cin >> t;
-    while (t--) testcase();
+    while(t--) testcase();
     return 0;
 }
