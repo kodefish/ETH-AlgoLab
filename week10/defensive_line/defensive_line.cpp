da853003b53ab0d29c7f27a1f640a9c9cfa57099
Danil Ivanov <danilivanov.ivanov@gmail.com>
2020-01-15 11:16:59 +0100

    defensive line 100

--- week10/defensive_line/defensive_line.cpp
+++ week10/defensive_line/defensive_line.cpp
@@ -1,35 +1,35 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 const int MEMO_INIT = -2;
 
-int dp(int start, int attackers, const int num_defenders, const std::vector<int> & sw, std::vector<std::vector<int>> & memo) {
-    if (start == num_defenders) return -1; // Attackers left, no more defenders -> illegal strategy
+int dp(int start, int attackers, const std::vector<int> & sw, std::vector<std::vector<int>> & memo) {
+    if (start == sw.size()) return -1;
 
-    //if (memo[attackers][start] != MEMO_INIT) return memo[attackers][start];
+    if (memo[attackers][start] != MEMO_INIT) return memo[attackers][start];
 
-    // If only one attacker left
-    int best_strategy = -1;
     if (attackers == 1) {
-        if (start == num_defenders - 1) return sw[start] > 0 ? sw[start] : -1;
+        if (start == sw.size() - 1) return sw[start] == -1 ? -1 : 1;
 
-        int strategy = sw[start] > 0 ? sw[start] : -1;
-        int next = dp(start + 1, attackers, num_defenders, sw, memo);
-        best_strategy = std::max(strategy, next);
-    } else {
-        for (int i = start; i < num_defenders; i++) {
-            if (sw[i] > 0) { // Only consider possible attack interval starts
-                int rec = dp(i + sw[i], attackers - 1, num_defenders, sw, memo);
-                if (rec < 0) continue; // Only consider legal strategies
-                int strategy = sw[i] + rec;
-                if (strategy > best_strategy) best_strategy = strategy;
-            }
-        }
+        int val = sw[start] - start + 1;
+        int rec = dp(start + 1, attackers, sw, memo);
+        memo[attackers][start] = std::max(val, rec);
+        return memo[attackers][start];
+    }
+
+    // Reccurence
+    int best_strategy = -1;
+    for (int i = start; i < sw.size(); i++) {
+        if (sw[i] == -1) continue; // No window start here
+
+        int val = sw[i] - i + 1;
+        int rec = dp(sw[i] + 1, attackers - 1, sw, memo);
+        if (rec > -1) best_strategy = std::max(best_strategy, val + rec);
     }
 
     memo[attackers][start] = best_strategy;
-
-    return best_strategy;
+    return memo[attackers][start];
 }
 
 void testcase() {
@@ -39,24 +39,31 @@ void testcase() {
     for (int i = 0; i < n; i++) std::cin >> defense_values[i];
 
     // Precomputation step -> sliding window
-    std::vector<int> sw(n, 0);
+    std::vector<int> sw(n, -1);
+    int intervals = 0;
     int i = 0, j = 0, sum = defense_values[i];
-    while (j < n || sum >= k) {
+    while (j < n) {
         // If interval is attackable, save it
         if (sum == k) {
-            sw[i] = j - i + 1;
+            sw[i] = j;
+            intervals++;
         }
 
         // Increment sliding window
-        if (sum < k && j < n) {
+        if (sum <= k) {
             sum += defense_values[++j]; // increment and add
         } else {
             sum -= defense_values[i++]; // remove and decrement
         }
     }
 
+    if (intervals < m) {
+        std::cout << "fail" << std::endl;
+        return;
+    }
+
     std::vector<std::vector<int>> memo(m+1, std::vector<int>(n, MEMO_INIT));
-    int best_strategy = dp(0, m, n, sw, memo);
+    int best_strategy = dp(0, m, sw, memo);
     if (best_strategy < 0) std::cout << "fail" << std::endl;
     else std::cout << best_strategy << std::endl;
 }
@@ -67,5 +74,6 @@ int main() {
     while (t--) testcase();
     return 0;
 }
+
 
 
