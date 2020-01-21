495390b71a36f4438d35d5c3b0feac8cf68269fa
Danil Ivanov <danilivanov.ivanov@gmail.com>
2020-01-21 12:05:35 +0100

    NY 100

--- week13/new_york/new_york.cpp
+++ week13/new_york/new_york.cpp
@@ -1,52 +1,62 @@
+// Credit goes to hlycklama
+// https://github.com/hlycklama/eth-algolab/blob/master/week12/potw/main.cpp
 #include <iostream>
-#include <vector>
-#include <climits>
+#include <queue>
+#include <set>
 #include <algorithm>
+#include <stack>
+
+void dfs(int v, std::multiset<int> &window, int s, int m, int k, std::deque<int> &hist,
+         const std::vector<std::deque<int>> &adj_list, const std::vector<int> &temp, std::set<int> &starting_points) {
+    hist.push_back(v);
+    int val = 0;
+
+    // Keep track of temps in the path, via a sorted deque
+    if (s >= m) {
+        window.erase(window.find(temp[*hist.begin()]));
+        val = hist.front();
+        hist.pop_front();
+    }
+    window.insert(temp[v]);
+
+    // If start of fesible journey, add as starting point
+    if (window.size() == m && *window.rbegin() - *window.begin() <= k) {
+        starting_points.insert(hist.front());
+    }
+
+    // go over reachable points in classic dfs manner
+    for (auto i : adj_list[v]) {
+        dfs(i, window, s + 1, m, k, hist, adj_list, temp, starting_points);
+    }
+
+    if (s >= m) {
+        hist.push_front(val);
+        window.insert(temp[val]);
+    }
+
+    window.erase(window.find(temp[v]));
+    hist.pop_back();
+}
 
 void testcase() {
     int n, m, k; std::cin >> n >> m >> k;
+
     std::vector<int> temp(n);
     for (int i = 0; i < n; i++) std::cin >> temp[i];
 
-    std::vector<int> next(n, n);
+    std::vector<std::deque<int>> adj_list(n);
     for (int i = 0; i < n - 1; i++) {
         int u, v; std::cin >> u >> v;
-        next[u] = v;
+        adj_list[u].push_back(v);
     }
 
-    // Check for feasibility starting at i that have outgoing routes
-    std::vector<int> starting_points;
-    int u = 0, v = u;
-    int min_i = u, max_i = u;
-    int j;
-    for (j = 0; j < m - 1 && next[v] < n; j++) {
-        v = next[v];
-        if (temp[v] < temp[min_i]) min_i = v;
-        if (temp[v] > temp[min_i]) max_i = v;
-    }
-    if (j == m - 1) {
-        while (v < n) {
-            int risk = temp[max_i] - temp[min_i];
-            if (risk <= k) starting_points.push_back(u); // Feasible trip of length m starting at i
-
-            v = next[v];
-            if (temp[v] < temp[min_i]) min_i = v;
-            if (temp[v] > temp[min_i]) max_i = v;
-
-
-            u = next[u];
-            if (min_i == u || max_i == u) {
-                for (int i = u; i != next[v]; i = next[i]) {
-                    if (temp[i] < temp[min_i]) min_i = v;
-                    if (temp[i] > temp[min_i]) max_i = v;
-                }
-            }
-        }
-    }
+    std::set<int> starting_points;
+    std::multiset<int> window;
+    std::deque<int> history;
+    dfs(0, window, 0, m, k, history, adj_list, temp, starting_points);
 
     if (starting_points.empty()) std::cout << "Abort mission" << std::endl;
     else {
-        std::sort(starting_points.begin(), starting_points.end());
         for (auto i : starting_points) std::cout << i << " ";
         std::cout << std::endl;
     }
